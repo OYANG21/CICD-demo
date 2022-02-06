1b389f077aad63c67b42c17e37520cf5132dbade
dexter-cc <dexter_zhang@trendmicro.com>
2022-02-06 17:23:20 +1100

    task/updated file

--- handler.py
+++ handler.py
@@ -2,13 +2,13 @@ import json
 import boto3
 
 def dynamo_itemcreate(team_name, team_country, team_desc, team_rating):
-        dynamodb = boto3.client('dynamodb')
-        db_name = "Challenge_DB"
-        dynamodb.put_item(TableName=db_name, Item={'country':{'S':team_country}, 'team':{'S':team_name}, 'desc':{'S':team_desc}, 'rating':{'S':team_rating}})
+    dynamodb = boto3.client('dynamodb')
+    db_name = "Challenge"
+    dynamodb.put_item(TableName=db_name, Item={'country':{'S':team_country}, 'team':{'S':team_name}, 'desc':{'S':team_desc}, 'rating':{'S':team_rating}})
 
 def lambda_handler(event, context):
-        print('Team Country: %s' % event['team_country'])
-        print('Team Name: %s' % event['team_name'])
-        print('Team Description: %s' % event['team_desc'])
-        print('Team Rating: %s' % event['team_rating'])
-        dynamo_itemcreate(event['team_name'], event['team_country'], event['team_desc'],event['team_rating'])
+    print('Team Country: %s' % event['team_country'])
+    print('Team Name: %s' % event['team_name'])
+    print('Team Description: %s' % event['team_desc'])
+    print('Team Rating: %s' % event['team_rating'])
+    dynamo_itemcreate(event['team_name'], event['team_country'], event['team_desc'],event['team_rating'])
